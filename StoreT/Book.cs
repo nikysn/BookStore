d34755b593caf7a97efdbe809b1ee954e03eae7f
Vadim <nikysn@yandex.ru>
2023-01-26 13:46:24 +0300

    Visual studio

---
+++ StoreT/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Title { get; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
