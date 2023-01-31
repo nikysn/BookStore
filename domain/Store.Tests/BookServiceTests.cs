d50e536bff36ac9e10e8e08b19e7d9448ffbc2e4
Vadim <nikysn@yandex.ru>
2023-01-31 12:49:10 +0300

    l Добавил новый тест BookService

--- domain/Store.Tests/BookServiceTests.cs
+++ domain/Store.Tests/BookServiceTests.cs
@@ -9,17 +9,17 @@ namespace Store.Tests
     public class BookServiceTests
     {
         [Fact]
-        public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+        public void MockGetAllByQuery_WithIsbn_CallsGetAllByIsbn()
         {
             var bookRepositoryStun = new Mock<IBookRepository>();
-
+            // далее происходит настройка с помощью метод Setup
             bookRepositoryStun.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))   // тут заглушка которая создает массив и возвращает его используя метод GetAllByIsbn
                 .Returns(new[] { new Book(1, "", "", "") });
 
             bookRepositoryStun.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>())) // Вторая заглушка которая создает массив и возвращает его используя метод GetAllByTitleOrAuthor
                 .Returns(new[] { new Book(2, "", "", "") });
 
-            var bookService = new BookService(bookRepositoryStun.Object);
+            var bookService = new BookService(bookRepositoryStun.Object); // вот этот Mock который в параметре, имеет тип IbookRepository
             var validIsbn = "ISBN 12345-67890";
 
             var actual = bookService.GetAllByQuery(validIsbn);
@@ -33,7 +33,7 @@ namespace Store.Tests
                                                                          //соответствует ли свойство Id элемента каждый элемент равен 1.
         }
 
-
+        
         [Fact]
         public void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
         {
@@ -57,6 +57,54 @@ namespace Store.Tests
                                                                          //Второй параметр, book => Assert.Equal(1, book.Id), представляет собой лямбда-выражение,
                                                                          //которое выполняет проверку Assert.Equal(1, book.Id) для каждого элемента коллекции и проверяет,
                                                                          //соответствует ли свойство Id элемента каждый элемент равен 1.
+        }
+
+        [Fact]
+        public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+        {
+            const int idOfIsbnSearch = 1;
+            const int idOfAuthorSearch = 2;
+
+            var bookRepository = new StubBookRepository();
+
+            bookRepository.ResultOfGetAllByIsbn = new[]
+            {
+                new Book(idOfIsbnSearch,"","","")
+            };
+
+            bookRepository.ResultOfGetAllByTitleOrAuthor = new[]
+            {
+                new Book(idOfAuthorSearch,"","","")
+            };
+
+            var bookService = new BookService(bookRepository);
+            var books = bookService.GetAllByQuery("ISBN 12345-67890");
+
+            Assert.Collection(books, book => Assert.Equal(idOfIsbnSearch, book.Id));
+        }
+
+        [Fact]
+        public void GetAllByQuery_WithTitle_CallsGetAllByTitleOrAuthor()
+        {
+            const int idOfIsbnSearch = 1;
+            const int idOfAuthorSearch = 2;
+
+            var bookRepository = new StubBookRepository();
+
+            bookRepository.ResultOfGetAllByIsbn = new[]
+            {
+                new Book(idOfIsbnSearch,"","","")
+            };
+
+            bookRepository.ResultOfGetAllByTitleOrAuthor = new[]
+            {
+                new Book(idOfAuthorSearch,"","","")
+            };
+
+            var bookService = new BookService(bookRepository);
+            var books = bookService.GetAllByQuery("Programming");
+
+            Assert.Collection(books, book => Assert.Equal(idOfAuthorSearch, book.Id));
         }
     }
 }
